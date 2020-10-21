011bc378e11cf08efce1b283e8852855c0a0bcf2
vladrus13 <vladrus13rus@yandex.ru>
2020-10-21 23:02:55 +0300

    Done Accumulator Back Lines

--- src/main/java/ru/accumulator/Accumulator.java
+++ src/main/java/ru/accumulator/Accumulator.java
@@ -1,5 +1,7 @@
 package ru.accumulator;
 
+import java.util.List;
+
 public interface Accumulator {
 
     long size();
@@ -7,4 +9,6 @@ public interface Accumulator {
     byte[] get(long position);
 
     void add(byte[] element);
+
+    List<byte[]> prove(long position);
 }
--- src/main/java/ru/accumulator/SmartBackLinesAccumulator.java
+++ src/main/java/ru/accumulator/SmartBackLinesAccumulator.java
@@ -3,13 +3,17 @@ package ru.accumulator;
 import ru.util.AccumulatorUtils;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SmartBackLinesAccumulator implements Accumulator {
     private long size;
     private final ArrayList<byte[]> S;
+    private final ArrayList<byte[]> elements;
 
     SmartBackLinesAccumulator() {
         S = new ArrayList<>();
+        elements = new ArrayList<>();
         size = 0;
     }
 
@@ -28,10 +32,32 @@ public class SmartBackLinesAccumulator implements Accumulator {
     public void add(byte[] element) {
         if ((size & (size - 1)) == 0) {
             S.add(null);
+            elements.add(null);
             size++;
         }
         byte[] sum = AccumulatorUtils.concatDigits(element, get(size - 1), get(AccumulatorUtils.d(size)));
         byte[] result = AccumulatorUtils.getSha256(sum);
         S.set((int) size, result);
+        elements.set((int) size, element);
+    }
+
+    public LinkedList<byte[]> prove(long position) {
+        LinkedList<byte[]> answer = new LinkedList<>();
+        prove(position, size, answer);
+        return answer;
+    }
+
+    public void prove(long i, long j, LinkedList<byte[]> answer) {
+        if (i > j) {
+            throw new IllegalArgumentException("Second argument more than first");
+        }
+        answer.addAll(List.of(elements.get((int) j), S.get((int) (j - 1)), S.get((int) AccumulatorUtils.pred(j))));
+        if (j > i) {
+            if (AccumulatorUtils.pred(j) >= i) {
+                prove(i, AccumulatorUtils.pred(j), answer);
+            } else {
+                prove(i, j - 1, answer);
+            }
+        }
     }
 }
