ed925962e429394e3e6b25a79efc294cb5817a58
ReachPeach <maxim.busyuk@gmail.com>
2020-10-23 20:12:58 +0300

    Probably fixed bugs

--- src/main/java/ru/accumulator/MerkleTree.java
+++ src/main/java/ru/accumulator/MerkleTree.java
@@ -158,9 +158,9 @@ public class MerkleTree {
     }
 
     private String getInnerVertexesHash(int index) {
-        if (index * 2 + 1 < hashed_data.size()) {
+        if (index * 2 + 1 < hashed_data.size() / 2) {
             return Arrays.toString(AccumulatorUtils.getSha256((
                     hashed_data.get(2 * index + 1) + hashed_data.get(2 * index + 2)).getBytes()));
-        } else return getLeaf(index);
+        } else return getLeaf(index - hashed_data.size() / 2);
     }
 }
